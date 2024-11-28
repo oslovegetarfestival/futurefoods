5651b45a2f41274ac33ce25e034461644c1e2002
Tommy Nordum <tommy@spindelvev.net>
2024-11-28 12:46:13 +0100

    Remove legacy redirects

--- web/next.config.js
+++ web/next.config.js
@@ -8,27 +8,27 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/omoss",
-        destination: "/om-oss",
-        permanent: true,
-      },
-      {
-        source: "/info-til-utstillere",
-        destination: "/bli-utstiller",
-        permanent: true,
-      },
-      {
-        source: "/nyhetsbrev",
-        destination:
-          "https://etiv-zcmp.maillist-manage.eu/ua/Optin?od=12ba7e11d358&zx=14ac23b5bd&tD=171b28f16c47a06&sD=171b28f16c4884d",
-        permanent: false,
-        basePath: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/omoss",
+  //       destination: "/om-oss",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/info-til-utstillere",
+  //       destination: "/bli-utstiller",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/nyhetsbrev",
+  //       destination:
+  //         "https://etiv-zcmp.maillist-manage.eu/ua/Optin?od=12ba7e11d358&zx=14ac23b5bd&tD=171b28f16c47a06&sD=171b28f16c4884d",
+  //       permanent: false,
+  //       basePath: false,
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
