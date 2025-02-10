9706677d7f7da1e1421f84dcb642d288ac77a85f
Tommy Nordum <tommy@spindelvev.net>
2025-02-10 10:36:36 +0100

    Add start time to frontpage hero

--- web/components/FrontpageHero/FrontpageHero.tsx
+++ web/components/FrontpageHero/FrontpageHero.tsx
@@ -1,4 +1,4 @@
-import { Section } from "components/Layout"
+import { Block, Section } from "components/Layout"
 import { AnimationHeader } from "components/AnimationHeader"
 import styles from "./FrontpageHero.module.scss"
 
@@ -15,8 +15,13 @@ export const FrontpageHero = () => {
           loading="eager"
           className={styles.logo}
         />
-        <div className="h2">5. mars 2025</div>
         <div className="h2">Factory Tøyen</div>
+        <Block top="1">
+          <div className="h2">5. mars 2025</div>
+        </Block>
+        <Block top="1">
+          <div className="h3">kl. 09.00-15.00</div>
+        </Block>
       </div>
     </Section>
   )
